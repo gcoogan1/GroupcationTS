40541b213eeab666a6a3aef29c29fd2e6810245b
Genevieve Coogan <gcoogan1@gmail.com>
2024-06-04 11:23:48 -0400

    merge

--- src/components/InlineButton/InlineButton.js
+++ src/components/InlineButton/InlineButton.js
@@ -7,27 +7,30 @@ import { defaultInlineStyles } from "./styles/InlineButton.styles";
 /**
  * This component renders a button based on type and size.
  * @important -> Needs a View wrapping the button to set width/height
- * 
+ *
  * @param {string} children required -> label of button
  * @param {function} onPress required -> onClick event for button
  * @param {string} buttonType optional -> primary, secondary (defaults to the primary type)
+ * @param {object} styles optional -> add styles to button
  * @param {boolean} isDisabled optional -> sets disabled state of button
  * @param {svg} iconRight optional -> displays icon (set color in icon element) to the right of text
  * @param {svg} iconLeft optional -> displays icon (set color in icon element) to the left of text
  * @returns {ReactElement} Renders a button.
- * 
+ *
  * @example  <InlineButton buttonType={"secondary"}>Click me</InlineButton>
  */
 const InlineButton = ({
   children,
   onPress,
   buttonType,
+  styles,
   isDisabled,
   iconRight,
   iconLeft,
 }) => {
   const getTextColor = (buttonType, isDisabled) => {
-    if (isDisabled || buttonType === "secondary")
+    if (isDisabled) return { color: theme.color.disabled.onBase };
+    if (buttonType === "secondary")
       return { color: theme.color.surface.onBaseSecondary };
     return { color: theme.color.surface.onBasePrimary };
   };
@@ -36,19 +39,20 @@ const InlineButton = ({
     <Pressable
       style={({ pressed, focused }) => [
         defaultInlineStyles.button,
+        { ...styles },
         pressed && defaultInlineStyles.pressed,
         focused && defaultInlineStyles.focused,
       ]}
       disabled={isDisabled}
       onPress={onPress}
     >
-      {!!iconRight && <Icon>{iconRight}</Icon>}
+      {!!iconLeft && <Icon>{iconLeft}</Icon>}
       <Text
         style={[defaultInlineStyles.text, getTextColor(buttonType, isDisabled)]}
       >
         {children}
       </Text>
-      {!!iconLeft && <Icon>{iconLeft}</Icon>}
+      {!!iconRight && <Icon>{iconRight}</Icon>}
     </Pressable>
   );
 };
--- util/helperFunctions/helperFunctions.js
+++ util/helperFunctions/helperFunctions.js
@@ -1,4 +1,16 @@
+const { parse, isValid, format } = require("date-fns");
+
 export const capitilaizeFirstLetter = (string) => {
-  if (!string) return
-  return string[0].toUpperCase() + string.slice(1)
-}
+  if (!string) return;
+  return string[0].toUpperCase() + string.slice(1);
+};
+
+export const dateIsValid = (dateString) => {
+  const parsedDate = parse(dateString, "MM/dd/yyyy", new Date());
+  return isValid(parsedDate) && format(parsedDate, "MM/dd/yyyy") === dateString;
+};
+
+export const timeIsValid = (timeString) => {
+  const parsedTime = parse(timeString, "HH:mm", new Date());
+  return isValid(parsedTime) && format(parsedTime, "HH:mm") === timeString;
+};
